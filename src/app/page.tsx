ff4c3b6ed01a41d3e59d474def088fed2158937f
anabelena <anabelen.aristah@gmail.com>
2025-03-04 14:09:57 -0800

    refactor: typo in Page component fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 import Team from "@/components/Team";
 
-
 export default function Page() {
-  return 
-  <div className="flex items-center justify-center m-4"> 
-        <Team/>
-  </div>;
+  return (
+    <>
+      <div className="flex items-center justify-center m-4">
+        <Team />
+      </div>
+    </>
+  );
 }
--- src/components/Datatable.tsx
+++ src/components/Datatable.tsx
@@ -1,11 +1,11 @@
-"use client"
+"use client";
 
 import {
   ColumnDef,
   flexRender,
   getCoreRowModel,
   useReactTable,
-} from "@tanstack/react-table"
+} from "@tanstack/react-table";
 
 import {
   Table,
@@ -14,16 +14,22 @@ import {
   TableHead,
   TableHeader,
   TableRow,
-} from "@/components/ui/table"
+} from "@/components/ui/table";
 
 interface DataTableProps<TData, TValue> {
-  columns: ColumnDef<TData, TValue>[]
-  data: TData[]
+  columns: ColumnDef<TData, TValue>[];
+  data: TData[];
 }
 
-export function DataTable<TData, TValue>({columns,data}: DataTableProps<TData,TValue>)
-{
-  const table = useReactTable({data,columns,getCoreRowModel: getCoreRowModel(),})
+export function DataTable<TData, TValue>({
+  columns,
+  data,
+}: DataTableProps<TData, TValue>) {
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
 
   return (
     <div className="rounded-md border">
@@ -41,7 +47,7 @@ export function DataTable<TData, TValue>({columns,data}: DataTableProps<TData,TV
                           header.getContext()
                         )}
                   </TableHead>
-                )
+                );
               })}
             </TableRow>
           ))}
@@ -70,5 +76,5 @@ export function DataTable<TData, TValue>({columns,data}: DataTableProps<TData,TV
         </TableBody>
       </Table>
     </div>
-  )
+  );
 }
